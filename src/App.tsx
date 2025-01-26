f7be055d63e9ce886c0db2ae2ef06eae00adcb7a
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-01-26 10:07:26 +0530

    unused content removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import InventoryTable from "./components/InventoryTable";
 import InventoryForm from "./components/InventoryForm";
 import Modal from "./components/Modal";
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface SelectProps {
     options: string[]
     selectedValue: string
